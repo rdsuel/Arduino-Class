c88f109fd104b4db8804939b8de8f950bdaf4095
Rick Suel <rdsuel@users.noreply.github.com>
2015-07-28 11:43:12 -0400

    Update exercise_1.ino

--- exercise_1/exercise_1.ino
+++ exercise_1/exercise_1.ino
@@ -1,48 +1,37 @@
 
-// Defines which pin the LED is connected to.
 int ledPin = 5;
-// Used to track that current state of the LED.
 bool ledState = LOW;
 
 void setup() {
-  // This function is executed once at startup.  
-  
-  // Configure the LED pin as an output.
   pinMode(ledPin, OUTPUT);
-
-  // Set up the serial window for 9600 baud.
   Serial.begin(9600);
 }
 
 void loop() {
-  // This loop is called repeatedly, so we will manage the blinking from here.
+
+  blinkLed();
+  printLedState();
   
-  // Toggle the LED.
-  if (ledState == LOW)
-  {
-    ledState = HIGH;
-  }
-  else
-  {
-    ledState = LOW;
-  }
-  setLed(ledState);
-  printLedState(ledState);
-  
-  // This delay will allow us to meet the 1Hz blink frequency (500ms on, 500ms off).
   delay(500);
 }
 
-void setLed(bool state)
+void blinkLed()
 {
-  digitalWrite(ledPin, state);
+    if (ledState == LOW)
+    {
+      ledState = HIGH;
+    }
+    else
+    {
+      ledState = LOW;
+    }
+    digitalWrite(ledPin, ledState);
 }
 
-void printLedState(bool state)
+void printLedState()
 {
-  // Create a string for sending to the serial window.
   String msg = "The LED state is:";
-  msg = msg + String(state);
+  msg = msg + String(ledState);
   Serial.println(msg);
 }
   
